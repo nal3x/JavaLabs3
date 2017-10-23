70d4e598470f6b4d741bf465cfbbda7a88714ad8
nalex <nalex@e7450>
2017-10-23 12:37:14 +0300

    second.

---
+++ src/Tokenizer.java
@@ -0,0 +1,10 @@
+/*
+Write an application that reads a line of text, tokenizes it using space 
+characters as delimiters and outputs only those words ending with 
+the letters "ED"
+ */
+
+
+public class Tokenizer {
+    
+}

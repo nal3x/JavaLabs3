f1a5e549fc2124093f395bf9fa88f451cbf4cdcb
nalex <nalex@e7450>
2017-10-23 12:36:30 +0300

    second

---
+++ src/Reverse.java
@@ -0,0 +1,9 @@
+/*
+Write an application that inputs a line of text, tokenizes the line with String
+method split and outputs the tokens in reverse order. Use space characters as 
+delimiters.
+*/
+
+public class Reverse {
+    
+}
